7969a41f8c5439a47d4fc0c95ba32e4b0fd8b5ab
Larry Chong <l4chong@gmail.com>
2016-10-22 11:30:16 -0400

    stuff

--- app/src/main/java/com/lc/giftthecode/donors/AddActivity.java
+++ app/src/main/java/com/lc/giftthecode/donors/AddActivity.java
@@ -3,6 +3,9 @@ package com.lc.giftthecode.donors;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.lc.giftthecode.R;
 
@@ -17,10 +20,32 @@ public class AddActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(mToolbar);
+        setTitle("");
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-        
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.donor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                sendDeliveries();
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void sendDeliveries() {
+        Toast.makeText(this, "Send to server", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
+++ app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
@@ -1,19 +1,24 @@
 package com.lc.giftthecode.donors;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.lc.giftthecode.MainActivity;
 import com.lc.giftthecode.R;
 
 public class DonorActivity extends AppCompatActivity {
 
     private FloatingActionButton mFab;
+    private Button mPickedUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,20 @@ public class DonorActivity extends AppCompatActivity {
                 NavigateToActvitiy();
             }
         });
+
+        mPickedUpButton = (Button) findViewById(R.id.pickedup);
+        mPickedUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+
+        getDelivery();
+    }
+
+    private void getDelivery(){
+
     }
 
     @Override
@@ -54,5 +73,33 @@ public class DonorActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void showDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Picked Up?");
+        alertDialogBuilder
+                .setMessage("Did the volunteer picked up the food donation?")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        pickedUp();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
 
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+    }
+
+    private void pickedUp(){
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+    }
 }
