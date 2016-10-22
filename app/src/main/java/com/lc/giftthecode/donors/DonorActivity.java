c4c44c0e212f01519114962b6a9eed4f1eb83cc0
Larry Chong <l4chong@gmail.com>
2016-10-22 12:25:59 -0400

    added networking stuff

--- app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
+++ app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
@@ -10,15 +10,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lc.giftthecode.MainActivity;
 import com.lc.giftthecode.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class DonorActivity extends AppCompatActivity {
 
     private FloatingActionButton mFab;
     private Button mPickedUpButton;
+    private final OkHttpClient client = new OkHttpClient();
+
+    private TextView mDonorName;
+    private TextView mDonorFood;
+    private TextView mDonorLocation;
+    private TextView mDonorSchedule;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +60,62 @@ public class DonorActivity extends AppCompatActivity {
             }
         });
 
+        mDonorName = (TextView) findViewById(R.id.donorname);
+        mDonorFood = (TextView) findViewById(R.id.donorfood);
+        mDonorLocation = (TextView) findViewById(R.id.donorlocation);
+        mDonorSchedule = (TextView) findViewById(R.id.donorschedule);
+
         getDelivery();
     }
 
     private void getDelivery(){
+        Request request = new Request.Builder()
+                .url(getString(R.string.BASE_URL) + "/donor/getdelivery")
+                .build();
 
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                DonorActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(DonorActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                DonorActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        Toast.makeText(DonorActivity.this, "success", Toast.LENGTH_SHORT).show();
+                        try {
+                            String responseData = response.body().string();
+                            JSONObject json = new JSONObject(responseData);
+
+//                            String responseData = response.body().string();
+//                            JSONObject json = new JSONObject(responseData);
+//                            final String owner = json.getString("name");
+
+                            mDonorName.setText(json.getString("donor"));
+                            mDonorFood.setText(json.getString("food"));
+                            mDonorLocation.setText(json.getString("location"));
+                            mDonorSchedule.setText(json.getString("time"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+            }
+        });
     }
 
     @Override
