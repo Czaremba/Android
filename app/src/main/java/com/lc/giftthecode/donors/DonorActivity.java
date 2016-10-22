a9ed7f7dd0525806d1f54b90517e8086ee30b23b
Larry Chong <l4chong@gmail.com>
2016-10-22 03:37:12 -0400

    test

--- app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
+++ app/src/main/java/com/lc/giftthecode/donors/DonorActivity.java
@@ -2,6 +2,7 @@ package com.lc.giftthecode.donors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.lc.giftthecode.R;
 
@@ -12,4 +13,10 @@ public class DonorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.edit_profile_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
 }
